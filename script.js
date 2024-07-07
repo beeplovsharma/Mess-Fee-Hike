a78521bc68f5abad0f73141e1312e816530f6574
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-07-07 15:24:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 document.querySelector('#Director').addEventListener('click', function() {
     // Replace 'yourEmailAddress' with the actual Gmail address
     var emailAddress = 'director@nitjsr.ac.in';
-    var subject = "Concern Regarding Increased Mess Fee and Pending Fee Remissions";
+    var subject = "Concern Regarding Increased Mess Fee and Tuition Fee";
     var body = `
 Respected Director Sir,
 
-This email is to express our growing concern regarding the recent increase in the mess fee. 
+This email is to express our growing concern regarding the recent increase in the mess & tuition fee. 
 
-Many students are already facing significant financial difficulties.  The sudden hike in the mess fee is overwhelming for those of us without scholarships or fee remissions. Moreover, several students who qualify for fee remission have not yet received it, which adds to their financial strain.
+Many students are already facing significant financial difficulties.  The sudden hike in the fee is overwhelming for those of us without scholarships or fee remissions. Moreover, several students who qualify for fee remission have not yet received it, which adds to their financial strain.
 
 We kindly request your immediate intervention to address this issue and reduce the fee to a more reasonable amount. 
 
